11f725e5cd27eb672098d7a366af4dc58b641c34
VaqueroFontenla <loret.vaquero@gmail.com>
2021-12-24 12:28:20 +0100

    Add express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("HOla Mundo");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.listen(port, () => {
+  console.log('Listen on', port);
+});
